5acca5122ad80b1330755355069cf63fe547bc3a
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-03-06 11:39:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import socket
 import pyfiglet
 import cv2
 from cv2 import dnn_superres
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw 
 import pyperclip
 
 
@@ -18,13 +18,13 @@ os.system("clear")
 #get the hostname will be used as username
 hostname = socket.gethostname()
 #banner
-banner = pyfiglet.figlet_format("PICTURE EDITOR/UPSCALLER")
+banner = pyfiglet.figlet_format("PICTURE EDITOR")
 
 #print the "welcome page" start
 print(banner)
 print("""
  ___________
-|    / \ ¤ |
+|    / \ ☼ |
 | /\/   \__|
 |/_________|
 """)
@@ -44,15 +44,17 @@ def help():
 
 #upscale function start
 def upscale():
+    pyfiglet.format("upscale")
+    sr = dnn_superres.DnnSuperResImpl_create()
     #Asking user for some basics infos : path of the picture, output path, upscale factor 
     print("image upscalling process")
     path = str(input("put the path of the picture : ")) 
     image = cv2.imread(path)  
     Image.open(path)
-    img_size = width, height #not usefull, maybe for other things but not in upscale function
+    width, height = Image.size #not usefull, maybe for other things but not in upscale function
     
     #time estimation code start
-    if width >= 400 and length >= 400: #if img is big so time is longer to process
+    if width >= 400 and  height >= 400: #if img is big so time is longer to process
         big = True
     else:
         big = False
@@ -71,10 +73,11 @@ def upscale():
     #use x2 algo if higher use algo 4 if higher retake the img re-use it upscale etc....
 
     if upscalling_ == 2: #if upscalling = 2 so use the x2 algorithm
-        modelressource == "/model/stuff.rb"
+        modelressource == "/ressources/x2.pb"
         sr.readModel(modelressource)
+        sr.setModel("edsr", 2)
         print("process started")
-        print("time estimation : ", estimation)
+        print("time estimation : ", estimation, "s")
         output = sr.upsample(image)
         cv2.imwrite(picture_name_output, result)
         see = str(input("do you want to see the result y/n"))
@@ -93,13 +96,14 @@ def upscale():
             quit()
     #if upscalling factor = 4 use x4 upscalling algorithm
     elif upscalling_ == 4:
-        modelressource == "/model/stuff_.rb" # will change later
+        modelressource == "/model/2x.rb" # will change later
         sr.readModel(modelressource)
+        sr.setModel("edsr", 4)
         print("process started")
         print("time estimation : ", estimation)
         output = sr.upsample(image)
         cv2.imwrite(picture_name_output, result)
-        see = str(input("d you want to see the result y/n"))
+        see = str(input("do you want to see the result y/n"))
 
         if see == "y":
             Image.show(picture_name_output)
@@ -114,6 +118,8 @@ def upscale():
             quit()
     #(Little hard) : if it's > 4 we search for a multiplier of this value (by 2) and finally discrease picture quality if its not a even number
     elif upscalling_ > 4:
+        print("[!] actually coding this pretty hard part check for next update")
+        """
         os.system("mkdir 'temp'")
         os.system("cp image /temp/")
         
@@ -131,27 +137,31 @@ def upscale():
 
     
 #upscale function end
+"""
 
 def bg():
+    pyfiglet.format("Black white switcher")
     path = str(input("set the path of the picture"))
+    picture_name_output = str(input("set the output path of your picture : "))
     Image.open(path)
     imgGray = color.rgb2gray(path)
-    Image.save("/grey/greyed.png")
+    Image.save(picture_name_output)
 
 
 def reize():
+
+    pyfiglet.format("reize")
 
     path = str(input("set the path of the img you want to reize : "))
     Image.open(path)
     print("reading your picture")
     output_path = str(input("set the output path with the picture name e.g : home/username/Desktop/life.png"))
-    image_size = width, height
+    width, heigh = Image.size
     x_reize = str(input("set the x pixels you want to reize :"))
     print("ok")
     y_reize = str(input("set the y pixel you want to reize"))
-    #TODO : add the process, because idk really how to do this.
-    image.resize((x_reize, y_reize))
-    Image.save(output_path)
+    Image.resize((x_reize, y_reize))
+    path.save(output_path)
     see = str(input("do you want to see the picture?"))
 
     if see == "y" or see =="Y":
@@ -160,41 +170,100 @@ def reize():
         time.sleep(1)
         print(banner)
 
+
 def text():
-    print("?")
+    width, heigh = Image.size
+    center = heigh//2, width//2
+    bottom = heigh//2, width//4
+    # Set differents position possible to put the text
+
+    pyfiglet.format("text editor")
+    print("Note: Since it's non gui we will ask you many question to place the text")
+
+    path = str(input("Set the path of your picture : "))
+    Image.open(path)
+    text = str(input("Set the text you want : "))
+    print("""set the front :
+            1: Arial
+            2: Impact
+            4: Algerian
+            5: Comic sans-serif
+    """)
+
+    font = int(input("font (1-6) : "))
+    size = int(input("set the size of your text (50-300)"))
+
+    if font == 1:
+        font = ImageFont.truetype('playfair/playfair-font.ttf', size)
+    elif font == 2:
+        font = ImageFont.truetype('ressources/ttf/x.ttf')
+    elif font == 3:
+        font = ImageFont.truetype('ressources/ttf/x.ttf')
+    elif font == 4:
+        font = ImageFont.truetype('ressources/ttf/x.ttf')
+    elif font == 4:
+        font = ImageFont.truetype('ressources/ttf/x.ttf')
+    else :
+        print("wrong input, quitting")
+        time.sleep(2)
+        quit()
+    
+    position = str(input("Set the position of your picture (bottom, top, center) : "))
+    r, g, b = int(input("Set rgb (enter 3 value")).spilt()
+    output_path = str(input("Set the output path of the picture"))
+    time.sleep(1)
+    print("[*] Processing... \n")
+    temp_edit = ImageDraw.Draw(path)
+    print("[*] Putting text...")
+    temp_edit.text((position), text, (r, g, b), font=font)
+    temp_edit.save(output_path)
+    print("[*] Done.")
+    see = str(input("do you want to see the picture? y/n"))
+
+    if see == "y":
+        Image.show(output_path)
+    elif see == "n":
+        print("ok\n")
+        print("[!] Quitting")
+        quit()
+    else:
+        print("wrong input case sensitive y or n")
+
     #TODO: Maybe I should see if we can import stuff to do this because it will be pretty hard
 
 while True:
-    cmd = raw_input(hostname"@image_editor -> ").lower()
+    cmd = input(hostname+"@image_editor -> ").lower()
 
-        if "help" in cmd:
-            help()
+    if "help" in cmd:
+        help()
 
-        elif "clear" in cmd:
-            os.system("clear")
+    elif "clear" in cmd:
+        os.system("clear")
 
-        elif "upscale" in cmd:
-            upscale()
+    elif "upscale" in cmd:
+        upscale()
 
-        elif "bg" in cmd:
-            bg() #call the function in the other file
+    elif "bg" in cmd:
+        bg() #call the function in the other file
 
-        elif "exit" in cmd:
-            quit()
-        elif "x" in cmd:
-            print("Add requests on Github...")
-            time.sleep(1)
-            print("") 
-        elif "reize" in cmd:
-            reize()
+    elif "exit" in cmd:
+        quit()
+    elif "x" in cmd:
+        print("Add requests on Github...")
+        time.sleep(1)
+        print("") 
+    elif "reize" in cmd:
+        reize()
 
-        elif "text" in cmd:
-            text()
-        elif "" in cmd:
-            print("please, type something.")
-        else:
-            print("wrong input")
-            os.system("clear")
-            print(banner)
+    elif "text" in cmd:
+        text()
+    elif cmd == "" or cmd == " ":
+        print("please, type something.")
+    else:
+        print("wrong input")
+        time.sleep(1)
+        os.system("clear")
+        time.sleep(1)
+        print(banner)
 
         #REPORT ANY ISSUE ON GITHUB
