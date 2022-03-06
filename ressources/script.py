8c8011b0db0d85b073dc1d55c769742fa67039bf
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-03-06 11:57:41 +0400

    Update script.py

--- ressources/script.py
+++ ressources/script.py
@@ -1,1 +1,9 @@
+import os 
+import sys
 
+arial = "/ressources/ttf/arial.ttf"
+impact = "/ressources/ttf/impact.ttf"
+algerian = "/ressources/ttf/algerian.ttf"
+comic-sans-serif = "/ressources/ttf/comc-sans-serif.ttf"
+
+#You can add you stuffs in ressources and add the path in this small script
