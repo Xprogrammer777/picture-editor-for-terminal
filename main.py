75b80d65f5bcacf21cced40088877f5a85249f82
Xprogrammer777 <76904094+Xprogrammer777@users.noreply.github.com>
2022-03-17 17:38:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -389,7 +389,7 @@ def compress():
         compress()
 
     else:
-        int("factor") #convert str to integer
+        float("factor") # Convert
         img.save(output_path, quality=95*factor) #Well do math if you don't understand
 
         see = str(input("[*] Do you want to see the result y/n"))
